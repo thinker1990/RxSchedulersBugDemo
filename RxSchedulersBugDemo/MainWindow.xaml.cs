ccdb312ceabffac117866f38b4328b44ab9eaac2
liumingkun <liumingkun@ucan.com>
2025-10-14 09:02:11 +0800

    Reproduce RxSchedulers bug.

---
+++ RxSchedulersBugDemo/MainWindow.xaml.cs
@@ -0,0 +1,25 @@
+﻿using ReactiveUI;
+using System.Reactive.Concurrency;
+using System.Windows;
+
+namespace TestReactiveUI;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+    {
+        RxSchedulers.MainThreadScheduler.Schedule(() =>
+        {
+            var currentThreadId = Environment.CurrentManagedThreadId;
+            testLabel.Content = $"Current thread Id: {currentThreadId}";
+        });
+    }
+}
